60a61369189ece6b9d34c40e0ff05245ef885fd3
Justyna Ataman <48988693+justynaataman@users.noreply.github.com>
2020-04-13 14:34:45 +0200

    Delete todo.js

--- todo.js
+++
@@ -1,6 +0,0 @@
-const dateElement = document.getElementById("date");
-
-const options = {weekday : "long", month:"short", day:"numeric"};
-const today = new Date();
-
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
