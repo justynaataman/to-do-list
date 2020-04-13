bd00bb478e4c6129480096b1cda5cd3dd416cc46
Justyna <j.ataman@tlen.pl>
2020-04-13 15:09:49 +0200

    trying setting buttons

--- js/todo.js
+++ js/todo.js
@@ -8,13 +8,34 @@ dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 const input = document.getElementById("input");
 
-var prosze = document.getElementById("one");
-prosze.innerHTML = 'start planning';
+var one = document.getElementById("one");
+var two = document.getElementById("two");
+var three = document.getElementById("three");
+var four = document.getElementById("four");
+var five = document.getElementById("five");
+var six = document.getElementById("six");
+var seven = document.getElementById("seven");
+var eight = document.getElementById("eight");
+var nine = document.getElementById("nine");
+
+var tasks = [];
+tasks.push(one);
+tasks.push(two);
+tasks.push(three);
+tasks.push(four);
+tasks.push(five);
+tasks.push(six);
+tasks.push(seven);
+tasks.push(eight);
+tasks.push(nine);
+
+one.innerHTML = 'start planning';
 
 input.addEventListener("keyup", function(event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
         const value = document.getElementById("input").value;
-        prosze.innerHTML = value.toString();
+        one.innerHTML = value.toString();
+        alert(tasks.length);
     }
 });
